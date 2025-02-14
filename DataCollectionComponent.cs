e7ed8086d930af3e588254ee6fbe7af3c5220ed4
Clement Bouvet <clement@cbouvet.fr>
2025-02-14 08:47:27 +0100

    parse settings into struct

--- DataCollectionComponent.cs
+++ DataCollectionComponent.cs
@@ -6,30 +6,28 @@ namespace DataCollectionWorld.wit.exports.edgee.protocols;
     Page, Track, User
     These functions are called by the Edgee runtime to get the HTTP request to make to the provider's API for each event type.
 */
+
+public struct Settings {
+    public string example = string.Empty;
+
+    public Settings(List<(string, string)> settings) {
+        for (int i = 0; i < settings.Count; i++) {
+            if (settings[i].Item1 == "example") {
+                this.example = settings[i].Item2;
+            }
+        }
+    }
+}
+
 public class DataCollectionImpl: IDataCollection {
     public static IDataCollection.EdgeeRequest Page(IDataCollection.Event e, List<(string, string)> settings) {
-        /*
-            settings is a list of tuple, which contains each key and secret for the provider
-            for example, if your component is set to use
-                [[components.data_collection]]
-                name = "my_component"
-                component = "outpout.wasm"
-                settings.test_project_id = "123456789"
-                settings.test_write_key = "abcdefg"
-            then
-            foreach (var settings in settings)
-            {
-                Console.WriteLine($"{settings.Item1}: {cred.Item2}");
-            }
-            will print:
-                test_project_id: 123456789
-                test_write_key: abcdefg
-         */
         string url = "https://example.com/";
+        Settings s = new Settings(settings);
         var headers = new List<(string, string)>
         {
             ("Authorization", "Bearer exampleToken123"),
-            ("Content-Type", "application/json")
+            ("Content-Type", "application/json"),
+            ("example", s.example)
         };
         string body = "{\"event\": \"page\"}";
         bool forward_client_headers = true;
@@ -37,10 +35,12 @@ public class DataCollectionImpl: IDataCollection {
     }
     public static IDataCollection.EdgeeRequest Track(IDataCollection.Event e, List<(string, string)> settings) {
         string url = "https://example.com/";
+        Settings s = new Settings(settings);
         var headers = new List<(string, string)>
         {
             ("Authorization", "Bearer exampleToken123"),
-            ("Content-Type", "application/json")
+            ("Content-Type", "application/json"),
+            ("example", s.example)
         };
         string body = "{\"event\": \"track\"}";
         bool forward_client_headers = true;
@@ -48,10 +48,12 @@ public class DataCollectionImpl: IDataCollection {
     }
     public static IDataCollection.EdgeeRequest User(IDataCollection.Event e, List<(string, string)> settings) {
         string url = "https://example.com/";
+        Settings s = new Settings(settings);
         var headers = new List<(string, string)>
         {
             ("Authorization", "Bearer exampleToken123"),
-            ("Content-Type", "application/json")
+            ("Content-Type", "application/json"),
+            ("example", s.example)
         };
         string body = "{\"event\": \"user\"}";
         bool forward_client_headers = true;
