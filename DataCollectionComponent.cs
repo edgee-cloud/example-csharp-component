a858e085b45ae13835029d5a9d06a9153ce1dddf
Alex Casalboni <alex@alexcasalboni.com>
2025-02-14 09:43:37 +0100

    Rename manifest fields, add example setting (#4)
    * rename manifest fields, add example setting
    
    * fix manifest version
    
    * parse settings into struct
    
    * gitignore: add output artefact
    
    * c#ify settings
    
    ---------
    
    Co-authored-by: Clement Bouvet <clement@cbouvet.fr>

--- DataCollectionComponent.cs
+++ DataCollectionComponent.cs
@@ -6,30 +6,29 @@ namespace DataCollectionWorld.wit.exports.edgee.protocols;
     Page, Track, User
     These functions are called by the Edgee runtime to get the HTTP request to make to the provider's API for each event type.
 */
+
+public struct Settings {
+    public string Example { get; }
+
+    public Settings(List<(string, string)> settings) {
+        Example = string.Empty;
+        foreach (var setting in settings) {
+            if (setting.Item1 == "example") {
+                Example = setting.Item2;
+            }
+        }
+    }
+}
+
 public class DataCollectionImpl: IDataCollection {
     public static IDataCollection.EdgeeRequest Page(IDataCollection.Event e, List<(string, string)> settings) {
-        /*
-            settings is a list of tuple, which contains each key and secret for the provider
-            for example, if your component is set to use
-                [[components.data_collection]]
-                name = "my_component"
-                component = "outpout.wasm"
-                settings.test_project_id = "123456789"
-                settings.test_write_key = "abcdefg"
-            then
-            foreach (var settings in settings)
-            {
-                Console.WriteLine($"{settings.Item1}: {cred.Item2}");
-            }
-            will print:
-                test_project_id: 123456789
-                test_write_key: abcdefg
-         */
         string url = "https://example.com/";
+        Settings s = new Settings(settings);
         var headers = new List<(string, string)>
         {
             ("Authorization", "Bearer exampleToken123"),
-            ("Content-Type", "application/json")
+            ("Content-Type", "application/json"),
+            ("example", s.Example)
         };
         string body = "{\"event\": \"page\"}";
         bool forward_client_headers = true;
@@ -37,10 +36,12 @@ public class DataCollectionImpl: IDataCollection {
     }
     public static IDataCollection.EdgeeRequest Track(IDataCollection.Event e, List<(string, string)> settings) {
         string url = "https://example.com/";
+        Settings s = new Settings(settings);
         var headers = new List<(string, string)>
         {
             ("Authorization", "Bearer exampleToken123"),
-            ("Content-Type", "application/json")
+            ("Content-Type", "application/json"),
+            ("example", s.Example)
         };
         string body = "{\"event\": \"track\"}";
         bool forward_client_headers = true;
@@ -48,10 +49,12 @@ public class DataCollectionImpl: IDataCollection {
     }
     public static IDataCollection.EdgeeRequest User(IDataCollection.Event e, List<(string, string)> settings) {
         string url = "https://example.com/";
+        Settings s = new Settings(settings);
         var headers = new List<(string, string)>
         {
             ("Authorization", "Bearer exampleToken123"),
-            ("Content-Type", "application/json")
+            ("Content-Type", "application/json"),
+            ("example", s.Example)
         };
         string body = "{\"event\": \"user\"}";
         bool forward_client_headers = true;
